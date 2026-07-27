fa5e4de36e40ad4389f8e924039d1fdb5799eee2
gizlivadinet-creator <gizlivadi.net@gmail.com>
2026-07-27 15:02:34 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { ErrorBoundary } from './ErrorBoundary.tsx';
 import { preloadModel } from './lib/search/embeddings';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
 
